f67df38be42fca95170ea1d0850e88a40b8bfba9
Mico Piira <Mico@es.(none)>
2014-07-06 23:09:57 +0300

    new file:   .gitignore 	new file:   Gemfile 	new file:   LICENSE.txt 	new file:   README.md new file:   Rakefile 	new file:   lib/scdownloader.rb B B A Ci D C

---
+++ lib/scdownloader.rb
@@ -0,0 +1,10 @@
+require "scdownloader/version"
+
+module Scdownloader
+  class Song
+    def test
+      puts "penis"
+    end
+  end
+end
+    
---
+++ lib/scdownloader/version.rb
@@ -0,0 +1,3 @@
+module Scdownloader
+  VERSION = "0.0.2"
+end
